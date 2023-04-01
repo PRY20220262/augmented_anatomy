d09e664e9da200d4ea636616f31975a3bbad26fa
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-03-31 21:46:35 -0500

    hu-03 agregado avance de pantalla forgot password

--- lib/pages/auth/request_pin.dart
+++ lib/pages/auth/request_pin.dart
@@ -1,3 +1,4 @@
+import 'package:augmented_anatomy/widgets/input.dart';
 import 'package:flutter/material.dart';
 
 class RequestPin extends StatelessWidget {
@@ -5,8 +6,36 @@ class RequestPin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Hoola', style: TextStyle(fontSize: 50),),),
+    Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          leading: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+        ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          SizedBox(
+            width: size.width * 0.8,
+            child: Text(
+              '¿Olvidaste tu contraseña?',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          const InputLabel(
+            label: 'Correo electrónico',
+            hint: 'example@email.com',
+          ),
+        ]),
+      ),
     );
   }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 TextTheme getAATextTheme() {
   return const TextTheme(
     titleLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
-    titleMedium: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-    titleSmall: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+    titleMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    titleSmall: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
 
-    labelLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-    labelMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
-    labelSmall: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
+    labelLarge: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w700),
+    labelMedium: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+    labelSmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
 
-    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
+    bodyMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal),
     
   );
 }
