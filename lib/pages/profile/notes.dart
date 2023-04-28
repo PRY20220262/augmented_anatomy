72726f4e7d520ac06995f179eb43267112c6d8f1
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-04-28 18:49:11 -0500

    hu-23 estructura base de componente

---
+++ lib/pages/profile/notes.dart
@@ -0,0 +1,47 @@
+import 'package:augmented_anatomy/utils/augmented_anatomy_colors.dart';
+import 'package:augmented_anatomy/widgets/error.dart';
+import 'package:flutter/material.dart';
+import 'package:augmented_anatomy/widgets/appbar.dart';
+
+class Notes extends StatefulWidget {
+  const Notes({super.key});
+
+  @override
+  State<Notes> createState() => _NotesState();
+}
+
+class _NotesState extends State<Notes> {
+  final Future<String> _calculation = Future<String>.delayed(
+    const Duration(seconds: 2),
+    () => 'Data Loaded',
+  );
+
+  void _refresh() {
+    // reload
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AAColors.backgroundGrayView,
+        appBar: AAAppBar(context, back: true, title: 'Mis apuntes'),
+        body: FutureBuilder(
+          future: _calculation,
+          builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return Text('hola');
+            } else if (snapshot.hasError) {
+              return ErrorMessage(onRefresh: _refresh);
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+        ),
+      ),
+    );
+  }
+}
