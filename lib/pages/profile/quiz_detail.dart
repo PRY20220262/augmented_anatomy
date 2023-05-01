333c5ebf2a470dfa1fc073c95e8c3a4673de0e0d
Gino Quispe Calixto <gino_21quispe@outlook.com>
2023-05-01 01:14:17 -0500

    Added clas quiz detail

---
+++ lib/pages/profile/quiz_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class QuizDetail extends StatefulWidget {
+  const QuizDetail({Key? key}) : super(key: key);
+
+  @override
+  State<QuizDetail> createState() => _QuizDetailState();
+}
+
+class _QuizDetailState extends State<QuizDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
